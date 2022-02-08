5f789137233a82d62e63d69d1ab02da1baebd527
Kim Novak <kimnovak187@gmail.com>
2022-02-08 09:46:24 +0100

    Add initial boilerplate setup

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import Layout from '@common/Layout';
+
+function App() {
+    return (
+        <>
+            <Layout>This is the main content</Layout>
+        </>
+    );
+}
+
+export default App;
---
+++ src/common/Layout/index.jsx
@@ -0,0 +1,11 @@
+function Layout({ children }) {
+    return (
+        <>
+            <header>This is the header</header>
+            <main>{children}</main>
+            <footer>This is the footer</footer>
+        </>
+    );
+}
+
+export default Layout;
